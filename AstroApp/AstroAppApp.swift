267aa57962eb060625d6aeac2e6e45c352263edc
Tristan <93832702+cheesetan@users.noreply.github.com>
2023-07-30 20:33:50 +0800

    Initial Commit

---
+++ AstroApp/AstroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroAppApp.swift
+//  AstroApp
+//
+//  Created by Tristan Chay on 30/7/23.
+//
+
+import SwiftUI
+
+@main
+struct AstroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
